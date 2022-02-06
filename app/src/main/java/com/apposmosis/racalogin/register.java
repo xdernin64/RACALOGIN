b8a2b194aeb71951bbcb2f33d01ca32a3f574c80
xdernin <xdernin@gmail.com>
2022-02-06 16:01:42 -0500

    Ahora si guarda datos en autenticacion y firebase!!!

--- app/src/main/java/com/apposmosis/racalogin/register.java
+++ app/src/main/java/com/apposmosis/racalogin/register.java
@@ -7,47 +7,75 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class register extends AppCompatActivity {
-    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://raca-app-default-rtdb.firebaseio.com/");
+    private EditText codigo,nombres,apellidos,celular,correo,contrasena;
+    public Button btnregistrar;
+    public TextView iralogin;
+
+    //datos a registrar
+    private String codigotext="";
+    private String nombrestext="";
+    private String apellidostext="";
+    private String celulartext="";
+    private String correotext="";
+    private String contrasenatext="";
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final TextView codigo=findViewById(R.id.txt_id);
-        final TextView nombres=findViewById(R.id.txt_nombres);
-        final TextView apellidos=findViewById(R.id.txt_apellidos);
-        final TextView celular=findViewById(R.id.txt_celular);
-        final TextView correo=findViewById(R.id.txt_correo);
-        final TextView contrasena=findViewById(R.id.txt_pwd);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase=FirebaseDatabase.getInstance().getReference();
 
-        final Button btnregistrar=findViewById(R.id.btn_register);
-        final TextView iralogin=findViewById(R.id.tv_iralogin);
+        codigo=(EditText) findViewById(R.id.txt_id);
+        nombres=(EditText)findViewById(R.id.txt_nombres);
+        apellidos=(EditText)findViewById(R.id.txt_apellidos);
+        celular=(EditText)findViewById(R.id.txt_celular);
+        correo=(EditText)findViewById(R.id.txt_correo);
+        contrasena=(EditText)findViewById(R.id.txt_pwd);
+
+        btnregistrar=findViewById(R.id.btn_register);
+        iralogin=findViewById(R.id.tv_iralogin);
+
+        //datos a registrar
 
         btnregistrar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                final String codigotext=codigo.getText().toString();
-                final String nombrestext=nombres.getText().toString();
-                final String apellidostext=apellidos.getText().toString();
-                final String celulartext=celular.getText().toString();
-                final String correotext=correo.getText().toString();
-                final String contrasenatext=contrasena.getText().toString();
+            public void onClick(View view) {
+                codigotext=codigo.getText().toString();
+                nombrestext=nombres.getText().toString();
+                apellidostext=apellidos.getText().toString();
+                celulartext=celular.getText().toString();
+                correotext=correo.getText().toString();
+                contrasenatext=contrasena.getText().toString();
+
                 if(codigotext.isEmpty() || nombrestext.isEmpty() || apellidostext.isEmpty()
                  || celulartext.isEmpty() || contrasenatext.isEmpty() || correotext.isEmpty()){
                     Toast.makeText(register.this, "Ingrese datos en todos los campos", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    databaseReference.child("Usuarios").addListenerForSingleValueEvent(new ValueEventListener() {
+                    registrarauth();
+
+                  /*  databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
                             if (snapshot.hasChild(codigotext)){
@@ -60,6 +88,9 @@ public class register extends AppCompatActivity {
                                 databaseReference.child("Usuarios").child(codigotext).child("Password").setValue(contrasenatext);
                                 databaseReference.child("Usuarios").child(codigotext).child("Correo").setValue(correotext);
                                 Toast.makeText(register.this, "El usuario se ha registrado correctamente", Toast.LENGTH_SHORT).show();
+
+                                registrarauth();
+
                             }
                         }
 
@@ -68,9 +99,8 @@ public class register extends AppCompatActivity {
 
                         }
 
-                    });
+                    });*/
 
-                    
                 }
             }
         });
@@ -80,6 +110,57 @@ public class register extends AppCompatActivity {
                 startActivity(new Intent(register.this,login.class));
             }
         });
+
+    }
+
+    private void registrarauth() {
+        mAuth.createUserWithEmailAndPassword(correotext,contrasenatext).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Map<String, Object> map= new HashMap<>();
+                    /*
+                                databaseReference.child("Usuarios").child(codigotext).child("Apellidos").setValue(apellidostext);
+                                databaseReference.child("Usuarios").child(codigotext).child("Nombres").setValue(nombrestext);
+                                databaseReference.child("Usuarios").child(codigotext).child("Celular").setValue(celulartext);
+                                databaseReference.child("Usuarios").child(codigotext).child("Password").setValue(contrasenatext);
+                                databaseReference.child("Usuarios").child(codigotext).child("Correo").setValue(correotext);
+                                Toast.makeText(register.this, "El usuario se ha registrado correctamente", Toast.LENGTH_SHORT).show();
+                                */
+                    map.put("Codigo",codigotext);
+                    map.put("Apellidos",apellidostext);
+                    map.put("Nombres",nombrestext);
+                    map.put("Celular",celulartext);
+                    map.put("Correo",correotext);
+                    map.put("Contrasena",contrasenatext);
+
+                    String id=mAuth.getCurrentUser().getUid();
+                    mDatabase.child("Usuarios").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if (task2.isSuccessful()){
+                                startActivity((new Intent(register.this,MainActivity.class)));
+                                finish();
+                            }
+                            else
+                            {
+                                Toast.makeText(register.this,"No se crearon los datos correctos", Toast.LENGTH_SHORT).show();
+                            }
+
+
+
+                        }
+                    });
+
+
+                }
+                else{
+                    Toast.makeText(register.this, "Usuario no se pudo registrar", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
     }
 }
